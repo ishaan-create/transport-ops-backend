cc64f1354b8f8e5ce70722f417b93e692bc2beaa
Ishaan Garg <ishaan@Ishaans-MacBook-Pro.local>
2026-06-16 18:02:00 +0530

    fix ssl config

--- db.js
+++ db.js
@@ -2,9 +2,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.DATABASE_URL && process.env.DATABASE_URL.includes('railway')
-    ? { rejectUnauthorized: false }
-    : false
+  ssl: { rejectUnauthorized: false }
 });
 
 pool.on('error', (err) => {
